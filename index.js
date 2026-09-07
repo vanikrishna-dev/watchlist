62a676c14638b86ccef06d3e2d20f7cef9dcb3cb
vanikrishna-dev <vanikrishna02@gmail.com>
2026-09-07 14:59:50 +0530

    searching for 1 result

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const searchButton = document.getElementById("searchButton")
 const searchInput = document.getElementById("searchInput")
+const mainCard = document.getElementById("mainCard")
+
 
 searchButton.addEventListener("click", () => {
     let movieName = searchInput.value
@@ -8,6 +10,22 @@ searchButton.addEventListener("click", () => {
     .then(res => res.json())
     .then(data => {
         console.log(data)
+        mainCard.innerHTML = `<div class="mainCardContent">
+        <img src=${data.Poster} >
+        <div id="cardContent">
+            <div id="title">
+                <p id="titleHeading">${data.Title}</p>
+                <p class="rating">⭐ ${data.imdbRating}</p>
+            </div>
+            <div id="highlightBox" > 
+                <p class="runtime" >${data.Runtime}</p>
+                <p class="genre" >${data.Genre}</p>
+                <button>Watchlist</button>
+            </div>
+            <p id="description">${data.Plot}</p>
+        </div>
+    </div>`
+
     })
 })
 
